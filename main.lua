b3c4cb719bf8c3b79068aedce7854534ffa94334
fang_yuan <fang_yuan_961@qq.com>
2024-05-01 22:06:36 +0800

    version 1, has finished gameover event.Then, need to use object to describe gameover event.

--- main.lua
+++ main.lua
@@ -1,5 +1,3 @@
-require("modules.cube")
-
 function love.load()
 	ball = {
 		vx = 15,
@@ -14,6 +12,7 @@ function love.load()
 		pos_x = 400,
 		pos_y = 640
 	}
+	GAME_OVER = 0
 end
 
 function love.update()
@@ -30,9 +29,19 @@ function love.update()
 	elseif (platform.pos_x + platform.width > love.graphics.getWidth()) then
 		platform.pos_x = love.graphics.getWidth() - platform.width
 	end
+
+	ball.vx, ball.vy = touch(ball, platform)
+	ball.x = ball.vx + ball.x
+	ball.y = ball.vy + ball.y
+
 end
 
 function love.draw()
+	if(GAME_OVER == 1) then
+		gameover_draw()
+		return
+	end
+
 	arr = {}
 	for i=2, 20, 2
 	do
@@ -42,13 +51,6 @@ function love.draw()
 	love.graphics.points(unpack(arr))
 
 	love.graphics.circle('fill', ball.x, ball.y, ball.r)
-
-	ball.vx, ball.vy = touch(ball, platform)
-	local vx, vy = ball.vx, ball.vy
-	ball.x = vx + ball.x
-	ball.y = vy + ball.y
-	
-
 	love.graphics.polygon('fill', platform.pos_x, platform.pos_y, 
 		platform.pos_x+platform.width, platform.pos_y, 
 		platform.pos_x+platform.width, platform.pos_y+platform.height, 
@@ -62,42 +64,114 @@ function touch(ball,platform)
 	if(ball.x+vx > love.graphics.getWidth()-ball.r or ball.x+vx < 0+ball.r) then 
 		vx = -vx
 	end
-	if(ball.y+vy > love.graphics.getHeight()-ball.r or ball.y+ball.vy < 0+ball.r) then 
+	if(ball.y+vy > love.graphics.getHeight()-ball.r) then 
+		gameover()
+	elseif (ball.y+ball.vy < 0+ball.r) then
 		vy = -vy
 	end
 
 	vx,vy = touch_platform(ball, platform, vx, vy);
-
-	return vx, vy
+	return vx,vy
 end
 
 function touch_platform(ball, platform, vx, vy)
-
-	if(ball.x >= platform.pos_x and ball.x <= platform.pos_x + platform.width) then
-		if(ball.y+ball.r >= platform.pos_y and ball.y <= platform.pos_y+platform.height
-			and ball.vy > 0) then
-			vy = -vy
-		elseif(ball.y >= platform.pos_y and ball.y - ball.r <= platform.pos_y+platform.height
-			and ball.vy < 0) then
-			vy = -vy
+	if(ball.y + ball.r >= platform.pos_y 
+	and ball.y - ball.r <= platform.pos_y + platform.height) then
+		if(vx > 0 
+		and ball.x + ball.r <= platform.pos_x 
+		and ball.x + ball.r + vx >= platform.pos_x) then
+			vx = -vx
+			ball.x = platform.pos_x - ball.r
+		elseif(vx < 0 
+		and ball.x - ball.r >= platform.pos_x + platform.width
+		and ball.x - ball.r + vx <= platform.pos_x + platform.width)  then
+			vx = - vx
+			ball.x = platform.pos_x + platform.width + ball.r
 		end
-	elseif(ball.y >= platform.pos_y and ball.y <= platform.pos_y + platform.height) then
-		if(ball.x+ball.r >= platform.pos_x and 
-			ball.x<= platform.pos_x + platform.width and
-			ball.vx > 0) then
-			vx = -vx
-		elseif(ball.x >= platform.pos_x and 
-			ball.x-ball.r <= platform.pos_x + platform.width and
-			ball.vx < 0) then
-			vx = -vx
+	end
+	if(ball.x + ball.r >= platform.pos_x 
+	and ball.x - ball.r <= platform.pos_x + platform.width) then
+		if(vy > 0
+		and ball.y + ball.r <= platform.pos_y	
+		and ball.y + ball.r + vy >= platform.pos_y) then
+			vy = -vy
+			ball.y = platform.pos_y - ball.r
+		elseif (vy < 0
+		and ball.y - ball.r >= platform.pos_y + platform.height
+		and ball.y - ball.r + vy <= platform.pos_y + platform.height) then
+			vy = -vy
+			ball.y = platform.pos_y + platform.height + ball.r
 		end
 	end
 	return vx, vy
 end
 
+-- ------------------
 
-function random_ball_speed(ball)
-	local vx = ball.vx + (love.math.random() * 10) % 5
-	local vy = ball.vy + (love.math.random() * 10) % 5
-	return vx, vy
+function start_ui_draw()
+	-- unfinshed
 end
+
+function gameover()
+	GAME_OVER = 1
+end
+
+function gameover_draw()
+	local font = love.graphics.newFont("test/ttf/Hack-Bold.ttf", 40)
+	love.graphics.setFont(font)
+	local text = "Game Over"
+	love.graphics.print(
+		text, 
+		(love.graphics.getWidth()- font:getWidth(text))/2, 
+		(love.graphics.getHeight()- font:getHeight())/2
+	)
+
+	local offsetY = 100
+	local cubeHW = 40
+	local cubeHH = 22
+
+	love.graphics.polygon(
+		'fill',
+		love.graphics.getWidth()/2 - cubeHW,
+		love.graphics.getHeight()/2 + offsetY - cubeHH,
+
+		love.graphics.getWidth()/2 + cubeHW,
+		love.graphics.getHeight()/2 + offsetY - cubeHH,
+
+		love.graphics.getWidth()/2 + cubeHW,
+		love.graphics.getHeight()/2 + offsetY + cubeHH,
+
+		love.graphics.getWidth()/2 - cubeHW,
+		love.graphics.getHeight()/2 + offsetY + cubeHH
+	)
+
+	local font2 = love.graphics.newFont("test/ttf/Hack-Bold.ttf", 16)
+	love.graphics.setFont(font2)
+	love.graphics.print({{0,0,0}, 'restart'}, 
+		love.graphics.getWidth()/2 - font2:getWidth('restart')/2,
+		love.graphics.getHeight()/2 + offsetY - font2:getHeight()/2
+	)
+
+	if(love.mouse.isDown(1)) then
+		local x,y = love.mouse.getPosition()
+		if(x >= love.graphics.getWidth()/2 - cubeHW 
+		and x <= love.graphics.getWidth()/2 + cubeHW 
+		and y >= love.graphics.getHeight()/2 + offsetY - cubeHH
+		and y <= love.graphics.getHeight()/2 + offsetY + cubeHH) then
+			restart()
+		end
+	end
+end
+
+function restart()
+	platform.pos_x, platform.pos_y = 400, 600
+	
+	local x1 = love.math.random() * love.graphics.getWidth()
+
+	if(x1 < ball.r) then x1 = ball.r end
+	if(x1 > love.graphics.getWidth()-ball.r) then x1 = love.graphics.getWidth()-ball.r end
+
+	ball.vx, ball.vy, ball.x, ball.y  = 15, 8, x1, 100
+	GAME_OVER = 0
+end
+
